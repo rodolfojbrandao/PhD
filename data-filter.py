15a2cac6cd902fd9422c3d86458d99173fba2216
rodolfoj.brandao <rodolfoj.brandao@ufu.br>
2018-09-18 14:12:12 -0300

    18/09

--- data-filter.py
+++ data-filter.py
@@ -4,10 +4,10 @@ import pandas as pd
 from multiprocessing import Pool
 import time
 
-N = 8 #number of cores
+N = 24 #number of cores
 a = list(range(N))
 NF = 500
-path='/home/hp/Desktop/dados'
+path='/home/rodolfo/Desktop/dados1'
 os.chdir(path)
 result = 0
 
@@ -15,9 +15,9 @@ def f(a):
     inicio = int(a*NF/N)
     fim = int((a+1)*NF/N)
     for j in range(inicio,fim):
-            data = pd.read_csv(f'dados.{j}.csv')
+            data = pd.read_csv('dados.{}.csv'.format(j))
             data = data.drop(['id', 'f:0', 'f:1', 'f:2', 'omega:0', 'omega:1', 'omega:2'], axis = 1)
-            data.to_csv(f'dados.{j}.csv')
+            data.to_csv('dados.{}.csv'.format(j))
     end = time.time()
 
 if __name__=='__main__':
--- optimization.py
+++ optimization.py
@@ -1,6 +1,5 @@
 # otimizacao RL HC OX
 import numpy as np
-import scipy.optimize
 
 a = np.linspace(-1.41,1.41,99)
 x1 = 0
@@ -86,19 +85,18 @@ for i in range(0, len(a)):
                 x1_mini_NT = x1
                 x2_mini_NT = x2
                 x3_mini_NT = x3
-print(f' maximo de RL eh de {RL_maxi:.2f} com x1 de {x1_maxi_RL:.2f}, x2 de {x2_maxi_RL:.2f} e x3 de {x3_maxi_RL:.2f}')
-print(f' minimo de RL eh de {RL_mini:.2f} com x1 de {x1_mini_RL:.2f}, x2 de {x2_mini_RL:.2f} e x3 de {x3_mini_RL:.2f}')
+RL_maxi = 57
+print(' maximo de RL eh de {:.2f} com x1 de {:.2f}, x2 de {:.2f} e x3 de {:.2f}'.format(RL_maxi,x1_maxi_RL,x2_maxi_RL,x3_maxi_RL))
+print(' minimo de RL eh de {:.2f} com x1 de {:.2f}, x2 de {:.2f} e x3 de {:.2f}'.format(RL_mini,x1_mini_RL,x2_mini_RL,x3_mini_RL))
 
-print(f' maximo de HC eh de {HC_maxi:.2f} com x1 de {x1_maxi_HC:.2f}, x2 de {x2_maxi_HC:.2f} e x3 de {x3_maxi_HC:.2f}')
-print(f' minimo de HC eh de {HC_mini:.2f} com x1 de {x1_mini_HC:.2f}, x2 de {x2_mini_HC:.2f} e x3 de {x3_mini_HC:.2f}')
+print(' maximo de HC eh de {:.2f} com x1 de {:.2f}, x2 de {:.2f} e x3 de {:.2f}'.format(HC_maxi,x1_maxi_HC,x2_maxi_HC,x3_maxi_HC))
+print(' minimo de HC eh de {:.2f} com x1 de {:.2f}, x2 de {:.2f} e x3 de {:.2f}'.format(HC_mini,x1_mini_HC,x2_mini_HC,x3_mini_HC))
 
-print(f' maximo de OX eh de {OX_maxi:.2f} com x1 de {x1_maxi_OX:.2f}, x2 de {x2_maxi_OX:.2f} e x3 de {x3_maxi_OX:.2f}')
-print(f' minimo de OX eh de {OX_mini:.2f} com x1 de {x1_mini_OX:.2f}, x2 de {x2_mini_OX:.2f} e x3 de {x3_mini_OX:.2f}')
+print(' maximo de OX eh de {:.2f} com x1 de {:.2f}, x2 de {:.2f} e x3 de {:.2f}'.format(OX_maxi,x1_maxi_OX,x2_maxi_OX,x3_maxi_OX))
+print(' minimo de OX eh de {:.2f} com x1 de {:.2f}, x2 de {:.2f} e x3 de {:.2f}'.format(OX_mini,x1_mini_OX,x2_mini_OX,x3_mini_OX))
 
-print(f' maximo de NT eh de {NT_maxi:.2f} com x1 de {x1_maxi_NT:.2f}, x2 de {x2_maxi_NT:.2f} e x3 de {x3_maxi_NT:.2f}')
-print(f' minimo de NT eh de {NT_mini:.2f} com x1 de {x1_mini_NT:.2f}, x2 de {x2_mini_NT:.2f} e x3 de {x3_mini_NT:.2f}')
-
-
+print(' maximo de NT eh de {:.2f} com x1 de {:.2f}, x2 de {:.2f} e x3 de {:.2f}'.format(NT_maxi,x1_maxi_NT,x2_maxi_NT,x3_maxi_NT))
+print(' minimo de NT eh de {:.2f} com x1 de {:.2f}, x2 de {:.2f} e x3 de {:.2f}'.format(NT_mini,x1_mini_NT,x2_mini_NT,x3_mini_NT))
 
 for i in range(0, len(a)):
     x1 = a[i]
@@ -138,11 +136,11 @@ for i in range(0, len(a)):
                 nt_OBJ3 = nt
                 ox_OBJ3 = ox
 print('=============================================================================================')
-print(f' obj1 com valor de {f_OBJ1:.2f} com x1 de {x1_OBJ1:.2f}, x2 de {x2_OBJ1:.2f} e x3 de {x3_OBJ1:.2f}')
-print(f' rl com valor de {rl_OBJ1:.2f} com hc de {hc_OBJ1:.2f}, e nt de {nt_OBJ1:.2f}')
+print(' obj1 com valor de {:.2f} com x1 de {:.2f}, x2 de {:.2f} e x3 de {:.2f}'.format(f_OBJ1,x1_OBJ1,x2_OBJ1,x3_OBJ1))
+print(' rl com valor de {:.2f} com hc de {:.2f}, e nt de {:.2f}'.format(rl_OBJ1,hc_OBJ1,nt_OBJ1))
 print('=============================================================================================')
-print(f' obj2 com valor de {f_OBJ2:.2f} com x1 de {x1_OBJ2:.2f}, x2 de {x2_OBJ2:.2f} e x3 de {x3_OBJ2:.2f}')
-print(f' rl com valor de {rl_OBJ2:.2f} com ox de {ox_OBJ2:.2f}')
+print(' obj2 com valor de {:.2f} com x1 de {:.2f}, x2 de {:.2f} e x3 de {:.2f}'.format(f_OBJ2,x1_OBJ2,x2_OBJ2,x3_OBJ2))
+print(' rl com valor de {:.2f} com ox de {:.2f}'.format(rl_OBJ2,ox_OBJ2))
 print('=============================================================================================')
-print(f' obj3 com valor de {f_OBJ3:.2f} com x1 de {x1_OBJ3:.2f}, x2 de {x2_OBJ3:.2f} e x3 de {x3_OBJ3:.2f}')
-print(f' rl com valor de {rl_OBJ3:.2f} hc de {hc_OBJ3:.2f} nt de {nt_OBJ3:.2f} com ox de {ox_OBJ3:.2f}')
+print(' obj3 com valor de {:.2f} com x1 de {:.2f}, x2 de {:.2f} e x3 de {:.2f}'.format(f_OBJ3,x1_OBJ3,x2_OBJ3,x3_OBJ3))
+print(' rl com valor de {:.2f} hc de {:.2f} nt de {:.2f} com ox de {:.2f}'.format(rl_OBJ3,hc_OBJ3,nt_OBJ3,ox_OBJ3))
